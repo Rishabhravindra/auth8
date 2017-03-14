2fc9f87375acba8ed6af631260b46cd733985b26
Rishabh Ravindra <rishabhravindra@gmail.com>
2017-03-14 06:58:13 -0400

    require mongoose and connect to mongodb

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 var app = express();
 
+
+//mongodb connection
+mongoose.connect("mongodb://localhost:27017/bookworm");
 // parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
